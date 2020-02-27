3a5b22d1214d5202e92b68f9e779117322dc3456
Kay Watanabe <kay.watanabe80@gmail.com>
2020-02-26 19:03:58 -0800

    set up featured products

--- front-page.php
+++ front-page.php
@@ -68,84 +68,46 @@ get_header();
 				<?php endif;?>
 				
 			</section>
+
 			<section class ="featured-products">
 				<h2>Featured Products</h2>
 				<?php if(get_field('home_featured') ):?>
-					<?php while(has_sub_field('home_featured')): 
-				$images = get_sub_field('featured_product');
-				$title = get_sub_field('featured_title');
-				$text = get_sub_field('featured_text');
-				$buttonText = get_sub_field('featured_button_label');
-				$size = 'medium'; // (thumbnail, medium, large, full or custom size)
-				?>
-				<?php if($images):
-				foreach($images as $image):
-				echo wp_get_attachment_image( $image, $size ); ?>
-				<div class="featured-banner-text-box">
-							<h3>
-							<?php echo $title?>
-							</h3>
-							<?php if($text):?>
-							<p>
-							<?php echo $text?>
-							<p>
-							<?php endif; ?>
-							<?php if($buttonText):
-								echo '<button src="get_permalink($image->ID)">'.$buttonText.'</button>'
-							?>
-							<?php endif; ?>
-				<?php endforeach;?>
-				<?php endif; ?>
-					</div>	
-				
-				<?php
-				
-				// 	$featured_product = new WP_Query( $args );
-
-				// 	if ( $featured_product->have_posts() ) : 
-
-				// 	echo '<div class="woocommerce columns-2"><ul class="products">';
-
-				// 	while ( $featured_product->have_posts() ) : $featured_product->the_post();
-
-				// 	$post_thumbnail_id     = get_post_thumbnail_id();
-				// 	$product_thumbnail     = wp_get_attachment_image_src($post_thumbnail_id, $size = 'shop-feature');
-				// 	$product_thumbnail_alt = get_post_meta( $post_thumbnail_id, '_wp_attachment_image_alt', true );
-				// ?>
-
-				// 	<li class="product">
-				// 		<a href="<?php the_permalink();?>">
-				// 			<img src="<?php echo $product_thumbnail[0];?>" alt="<?php echo $product_thumbnail_alt;?>">
-				// 			<h3 class="woocommerce-loop-product__title"><?php the_title();?></h3>
-				// 			<button class="yellow-but">VIEW PRODUCT</button>
-				// 		</a>    
-				// 	</li>
-				<?php 
-				// endwhile; 
-
-				// echo '</ul></div>';
-
-				// endif; 
-						
-				// wp_reset_query();
-				// ?>  
+				<?php while(has_sub_field('home_featured')): 
+					$title = get_sub_field('featured_title');
+					$text = get_sub_field('featured_text');
+					$buttonText = get_sub_field('featured_button_label');
+					$posts = get_sub_field('featured_image');
+					if($posts):?>
+					<div class = "featured-image">
+					<?php foreach($posts as $post):?>	
+					<?php setup_postdata($post); ?>
+					<?php the_post_thumbnail( 'medium' );?>
+					<?php endforeach;
+					wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+					</div>
+					<?php endif;?>
+				<div class="featured-text-box">
+				<h3><?php echo $title ?></h3>
+				<p><?php echo $text ?><p>
+				<button><a href = "<?php the_permalink(); ?>"><?php echo $buttonText ?></a></button>
+				</div>
 				<?php endwhile; ?>
 				<?php endif;?>        
-<!-- Featured products loop -->  
+				<!-- Featured products loop -->  
 			</section>
 			
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		// while ( have_posts() ) :
+		// 	the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+		// 	get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-		endwhile; // End of the loop.
+		// 	// If comments are open or we have at least one comment, load up the comment template.
+		// 	if ( comments_open() || get_comments_number() ) :
+		// 		comments_template();
+		// 	endif;
+		// endwhile; // End of the loop.
 		?>
 
 	

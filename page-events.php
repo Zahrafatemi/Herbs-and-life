1be9bef3f4c80519665f68359e317b4c71d50af7
Donna Dominguez Gamboa <56888091+dm-gamboa@users.noreply.github.com>
2020-02-29 07:04:17 -0800

    Update page-events.php
    Indentation and coding style

--- page-events.php
+++ page-events.php
@@ -17,58 +17,47 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			
+			global $post;
 
-		endwhile; // End of the loop.
-		?>
+			$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
 
-		<?php
+			$args = array(
+				'posts_per_page' => '-1',
+				'product_cat' => 'events',
+				'post_type' => 'product',
+				'orderby' => 'title',
+			);
 		
-		global $post;
+			$query = new WP_Query( $args );
+			if( $query->have_posts()) : 
+				while( $query->have_posts() ) : 
+					$query->the_post();
+			?>
 
-		$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
-
-		?>
-
-		<?php
-		$args = array(
-			'posts_per_page' => '-1',
-			'product_cat' => 'events',
-			'post_type' => 'product',
-			'orderby' => 'title',
-		);
-	
-		$query = new WP_Query( $args );
-		if( $query->have_posts()) : 
-			while( $query->have_posts() ) 
-				: $query->the_post();
-		?>
-		<div class="single-event">
-			<span class="excerpt"><?php the_excerpt() ?></span>
-			<span class="title">
-				<a href="<?php the_permalink() ?>">
-					<?php the_title(); ?>
-				</a>
-			</span>
-			<?php the_post_thumbnail('event-thumbnail'); ?>
-		</div>
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
-		<?php else:  ?>
-		<p>
-			<?php _e( 'No Products' ); ?>
-		</p>
-		<?php endif; ?>
+					<div class="single-event">
+						<span class="excerpt"><?php the_excerpt() ?></span>
+						<a class="title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+						<?php the_post_thumbnail('event-thumbnail'); ?>
+					</div><!--.single-event-->
+				<?php 
+				endwhile;
+				wp_reset_postdata();
+			else: ?>
+				<p><?php _e( 'No Products' ); ?></p>
+			<?php endif; ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	

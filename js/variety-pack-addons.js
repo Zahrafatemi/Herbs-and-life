004006df4a3b86bdeb7743c1351021a9dd4befea
Donna Dominguez Gamboa <56888091+dm-gamboa@users.noreply.github.com>
2020-02-29 22:14:33 -0800

    Add toggle to Variety Pack product addons

--- js/variety-pack-addons.js
+++ js/variety-pack-addons.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
-    $('body').on('change', 'input[name="addon-185-custom-packaging-0[]"]', function() {
-        console.log(`${$(this).attr('name')}`);
+    $( 'body' ).on( 'change', 'input[name="addon-185-custom-packaging-0[]"]', function() {
+        $( '.wc-pao-addon-container' ).toggle();
+        $( '.wc-pao-addon-custom-packaging' ).show();
     });
 });

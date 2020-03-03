510a39b31ff80718a692525b20f2433fe488f873
Zahra Fatemi <fatemi01@gmail.com>
2020-03-03 14:54:35 -0800

    related and product fields got filtered for event page

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -118,15 +118,16 @@ if ( post_password_required() ) {
 
 				
 
-	
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
-	do_action( 'woocommerce_after_single_product_summary' );
+	if((!has_term('events', 'product_cat', $post->ID))):
+		/**
+		 * Hook: woocommerce_after_single_product_summary.
+		 *
+		 * @hooked woocommerce_output_product_data_tabs - 10
+		 * @hooked woocommerce_upsell_display - 15
+		 * @hooked woocommerce_output_related_products - 20
+		 */
+		do_action( 'woocommerce_after_single_product_summary' );
+	endif;
 	?>
 </div>
 

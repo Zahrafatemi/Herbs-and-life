f375b0996658a90545fd3e469ae85ade9a619707
Donna Dominguez Gamboa <56888091+dm-gamboa@users.noreply.github.com>
2020-03-04 14:38:20 -0800

    Add isotope filter elements

--- index.php
+++ index.php
@@ -18,6 +18,17 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+			<div class="filters">
+				<div class="ui-group">
+					<h3>Type</h3>
+					<div class="button-group js-radio-button-group" data-filter-group="color">
+						<button class="button is-checked" data-filter="">Any</button>
+						<button class="button" data-filter=".news">News</button>
+						<button class="button" data-filter=".recipe">Recipes</button>
+					</div><!--.button-group js-radio-button-group-->
+				</div><!--.ui-group-->
+			</div><!--.filters-->
+
 		<?php
 		if ( have_posts() ) :
 
@@ -29,12 +40,14 @@ get_header();
 				<?php
 			endif;
 
-			function tagsToClass() {
+			function tagsToString() {
 				$tags = [];
 				$tagObjects = get_the_tags();
 
-				foreach( $tagObjects as $tagObject ) {
-					$tags[] = $tagObject->name;
+				if( $tagObjects ) {
+					foreach( $tagObjects as $tagObject ) {
+						$tags[] = $tagObject->name;
+					}
 				}
 
 				if( $tags ) {
@@ -49,12 +62,7 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', 'blog' );
 
 			endwhile;
 

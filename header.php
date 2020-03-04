84a3912cc3af59e2bea1aa3f61fd15c015e2ec32
Kay Watanabe <kay.watanabe80@gmail.com>
2020-03-04 01:34:56 -0800

    Remove comment out for myaccount text due to error on live

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
 			<!-- Account from Woocommerce See woocommerce.php -->	
 			<span class="account">
 				<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('My Account',''); ?>"><img src= "<?php echo get_template_directory_uri();?>/images/myaccount-logo.png">
-					<?php// _e('My Account',''); ?>
+					<?php _e('My Account',''); ?>
 				</a>
 			</span><!--.account-->
 			</div><!--.icon-wrapper-->

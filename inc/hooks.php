18d29d0dd5046b02b12668df4643411622c75d6f
DONNA Gamboa <A01207448@ad.bcit.ca>
2020-03-05 14:58:27 -0800

    Update excerpt hooks

--- inc/hooks.php
+++ inc/hooks.php
@@ -6,13 +6,39 @@
  */
 
 /* --------------------------------------------------
- * # Blog Page
+ * # Excerpt
  * -------------------------------------------------- /
 
  /**
- * Replace excerpt from [...] to link
+ * Change excerpt length
  */
-function hl_replace_excerpt_more( $more ) {
+function hl_excerpt_length() {
+	return 20;
+}
+
+/**
+ * Replace [...] in excerpt
+ */
+function hl_excerpt_more() {
 	return ' ... <span class="read-more">Read more</span>';
 }
-add_action( 'excerpt_more', 'hl_replace_excerpt_more' );
+
+/**
+ * Excerpt filters
+ */
+function hl_excerpt(){
+	global $post;
+	$excerpt_text = get_the_excerpt();
+	if( function_exists( 'get_field' ) ){
+		// If ACF description field exists, use this as excerpt text instead
+		$description = get_field( 'description' );
+		if( $description ){
+			$excerpt_text = $description;
+		}
+	}
+	$excerpt_length = hl_excerpt_length();
+	$excerpt_more = hl_excerpt_more();
+	$excerpt = wp_trim_words( $excerpt_text, $excerpt_length, $excerpt_more );
+	return $excerpt;
+}
+add_filter( 'the_excerpt', 'hl_excerpt' );	

2ddbcb78045c2cff9b831c05d32d97e86e99d256
DONNA Gamboa <A01207448@ad.bcit.ca>
2020-03-05 15:44:53 -0800

    Remove comments in single

--- page.php
+++ page.php
@@ -14,27 +14,23 @@
 
 get_header();
 ?>
+	<main id="main" class="site-main">
+		<div class="wrapper">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+				get_template_part( 'template-parts/content', 'page' );
 
-	<div class="wrapper">
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
 		</div><!-- .wrapper -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
 <?php
 get_footer();

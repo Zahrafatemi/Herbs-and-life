00b69c94677c32daa125ac22310077261ba3f6e9
DONNA Gamboa <A01207448@ad.bcit.ca>
2020-03-05 11:01:20 -0800

    Update blog page to match mockup structure

--- inc/hooks.php
+++ inc/hooks.php
@@ -6,7 +6,13 @@
  */
 
 /* --------------------------------------------------
- * ## Index Hooks
+ * # Blog Page
  * -------------------------------------------------- /
 
- 
+ /**
+ * Replace excerpt from [...] to link
+ */
+function hl_replace_excerpt_more( $more ) {
+	return ' ... <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">Read more</a> ';
+}
+add_action( 'excerpt_more', 'hl_replace_excerpt_more' );

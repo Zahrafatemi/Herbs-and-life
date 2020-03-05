0acc8c1e02f205742454d66e24c8f5ef01aa5192
Kyungmi Moon <kyungmimoondesign@gmail.com>
2020-03-05 10:45:25 -0800

    Fix white gap on the right side

--- js/slick-settings.js
+++ js/slick-settings.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function($){
 
+    // Hero banner slider
     $('.slider').slick({
+        arrow: false,
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000, // speed is in milliseconds
+        speed: 300
+    });
+
+    // Award slider
+    $('.award-slider').slick({
         arrow: true,
         dots: false,
         infinite: true,
@@ -10,5 +23,6 @@ jQuery(document).ready(function($){
         autoplaySpeed: 5000, // speed is in milliseconds
         speed: 300
     });
+
 
 });

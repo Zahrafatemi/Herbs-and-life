70f4c3d187b4546bd6df73a6e0b9436ffedd8ca5
Donna Dominguez Gamboa <56888091+dm-gamboa@users.noreply.github.com>
2020-03-06 18:38:39 -0800

    Add infinite scroll theme support

--- functions.php
+++ functions.php
@@ -84,9 +84,30 @@ if ( ! function_exists( 'herblife_setup' ) ) :
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
+		
+
 	}
 endif;
 add_action( 'after_setup_theme', 'herblife_setup' );
+
+function hl_blog_post_infinite_scroll(){
+	while( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', 'blog' );
+	}
+}
+
+function herblife_infinite_scroll() {
+	add_theme_support( 'infinite-scroll', array(
+		'container' 		=> 'infinite-scroll',
+		'type' 				=> 'click',
+		'footer_widgets' 	=> true,
+		'render' 			=> 'hl_blog_post_infinite_scroll',
+		'wrapper'			=> false,
+		'posts_per_page'	=> 3
+	) );
+}
+add_action( 'after_setup_theme', 'herblife_infinite_scroll' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
--- index.php
+++ index.php
@@ -21,83 +21,92 @@ get_header();
 			</header>
 
 			<div class="wrapper">
-
 				<?php
-				function getFilters( $category='category', $type='radio' ){
-					$filters = '';
-
-					$categorySlug = get_category_by_slug( $category );
-					
-					$args = array(
-						'parent' => $categorySlug->term_id
-					);
-
-					$subcategories = get_categories( $args );
-
-					if( $subcategories ){
-						// Default option for filter group
-						if( $type == 'dropdown' ){
-							$filters .= "<option class='default' name=" . $category . " data-filter='' selected='selected'>Choose Option</option>\r\n";
-						}else{
-							$filters .= "<li class='filter-option' data-filter=''>\r\n";
-							$filters .= "<input class='default' type=" . $type . " id='any-" . $category . "' name=" . $category . " checked='checked' />\r\n";
-							$filters .= "<label for='any-" . $category ."'>Any</label>\r\n";
-							$filters .= "</li>\r\n";
-						}
-					}
-
-					foreach( $subcategories as $subcategory ){
-						if( $type == 'dropdown' ){
-							$filters .= "<option name=" . $category . " data-filter=." . $subcategory->slug . ">" . $subcategory->name . "</option>\r\n";
-						}else{
-							$filters .= "<li class='filter-option' data-filter=." . $subcategory->slug . ">\r\n";
-							$filters .= "<input type=" . $type . " id=" . $subcategory->slug . " name=" . $category . " />\r\n";
-							$filters .= "<label for=" . $subcategory->slug . ">" . $subcategory->name . "</label>\r\n";
-							$filters .= "</li>\r\n";
-						}
-					}
-
-					return $filters;
-				}
+				if( have_posts() ):
 				?>
-
-				<div class="filters">
-					<ul class="main filter-group" data-filter-cat="type">
-						<?php echo getFilters(); ?>
-					</ul><!--.main-->
-
-					<div class="recipes-filters">
-						<div class="cuisine filter-group" data-filter-cat="cuisine">
-							<label class="filter-category" for="cuisine">Cuisine: </label>
-							<select class="filter-option" id="cuisine">
-								<?php echo getFilters( "cuisine", "dropdown" );?>
-							</select>						
-						</div><!--.cuisine-->
-
-						<ul class="meal filter-group" data-filter-cat="meal">
-							<span class="filter-category">Meal:</span>
-							<?php echo getFilters( "meal" );?>
-						</ul><!--.meal-->
-
-						<ul class="ingredients filter-group" data-filter-cat="ingredients">
-							<?php echo getFilters( "ingredients" );?>
-						</ul><!--.ingredients-->						
-					</div>
-
-
-				</div><!--.filters-->
-				
-				<div class="blog-feed">
 					<?php
-					while( have_posts() ){
-						the_post();
-						get_template_part('template-parts/content', 'blog');
-					}					
-					?>
-				</div><!--.blog-feed-->
+					function getFilters( $category='category', $type='radio' ){
+						$filters = '';
 
-				<button class="load-more">Load More</button>
+						$categorySlug = get_category_by_slug( $category );
+						
+						$args = array(
+							'parent' => $categorySlug->term_id
+						);
+
+						$subcategories = get_categories( $args );
+
+						if( $subcategories ){
+							// Default option for filter group
+							if( $type == 'dropdown' ){
+								$filters .= "<option class='default' name=" . $category . " data-filter='' selected='selected'>Choose Option</option>\r\n";
+							}else{
+								$filters .= "<li class='filter-option' data-filter=''>\r\n";
+								$filters .= "<input class='default' type=" . $type . " id='any-" . $category . "' name=" . $category . " checked='checked' />\r\n";
+								$filters .= "<label for='any-" . $category ."'>Any</label>\r\n";
+								$filters .= "</li>\r\n";
+							}
+						}
+
+						foreach( $subcategories as $subcategory ){
+							if( $type == 'dropdown' ){
+								$filters .= "<option name=" . $category . " data-filter=." . $subcategory->slug . ">" . $subcategory->name . "</option>\r\n";
+							}else{
+								$filters .= "<li class='filter-option' data-filter=." . $subcategory->slug . ">\r\n";
+								$filters .= "<input type=" . $type . " id=" . $subcategory->slug . " name=" . $category . " />\r\n";
+								$filters .= "<label for=" . $subcategory->slug . ">" . $subcategory->name . "</label>\r\n";
+								$filters .= "</li>\r\n";
+							}
+						}
+
+						return $filters;
+					}
+					?>
+
+					<div class="filters">
+						<ul class="main filter-group" data-filter-cat="type">
+							<?php echo getFilters(); ?>
+						</ul><!--.main-->
+
+						<div class="recipes-filters">
+							<div class="cuisine filter-group" data-filter-cat="cuisine">
+								<label class="filter-category" for="cuisine">Cuisine: </label>
+								<select class="filter-option" id="cuisine">
+									<?php echo getFilters( "cuisine", "dropdown" );?>
+								</select>						
+							</div><!--.cuisine-->
+
+							<ul class="meal filter-group" data-filter-cat="meal">
+								<span class="filter-category">Meal:</span>
+								<?php echo getFilters( "meal" );?>
+							</ul><!--.meal-->
+
+							<ul class="ingredients filter-group" data-filter-cat="ingredients">
+								<?php echo getFilters( "ingredients" );?>
+							</ul><!--.ingredients-->						
+						</div>
+
+
+					</div><!--.filters-->
+					
+					<div id="infinite-scroll" class="blog-feed">
+						<?php
+						while ( have_posts() ) {
+							the_post();
+
+							get_template_part( 'template-parts/content', 'blog' );
+						}
+						?>
+					</div><!--#infinite-scroll.blog-feed-->
+				<?php
+				else:
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+				?>
 		</div><!-- .wrapper -->
+		<div id="infinite-handle">
+			<button class="load-more button">Load More</button>
+		</div><!--#infinite-handle-->
 	</main><!-- #main -->
 
 <?php

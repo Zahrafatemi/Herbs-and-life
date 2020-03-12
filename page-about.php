6caf3f5859faf58f503c4d8a8654f2ce585b61bf
Kyungmi Moon <kyungmimoondesign@gmail.com>
2020-03-12 02:03:51 -0700

    Style about page banner

--- page-about.php
+++ page-about.php
@@ -21,25 +21,25 @@ get_header();
 			<h1 class="page-title title-on-banner"><?php the_title(); ?></h1>
 
 			<?php if( function_exists( 'get_field' ) ):?>
-				<div class="about-image">
+				<div class="about-banner">
 					<?php 
 					if(get_field('about_us_image')):
 						echo wp_get_attachment_image( get_field('about_us_image') , 'full' );
 					endif;
 					?>
-				</div><!--.about-image-->
+
+					<div class="about-banner-text-box">
+						<?php if( function_exists( 'get_field' ) ){
+							$mission_title 	= get_field( 'mission_title' );
+							$mission_text	= get_field( 'mission' ); 
+						}?>
+						<h3 class="mission-title"><?php if( $mission_title ){ echo $mission_title; }?></h3>
+						<p class="mission-text description"><?php if( $mission_text ){ echo $mission_text; }?></p>
+					</div><!--.about-banner-text-box-->
+				</div><!--.about-banner-->
 			<?php endif;  ?>
 	 <div class="wrapper pattern01">
-			<section class="about-intro">
-				<?php if( function_exists( 'get_field' ) ){
-					$mission_title 	= get_field( 'mission_title' );
-					$mission_text	= get_field( 'mission' ); 
-				}?>
-				<div class="about-banner-text-box">
-					<h3 class="mission-title"><?php if( $mission_title ){ echo $mission_title; }?></h3>
-					<p class="mission-text description"><?php if( $mission_text ){ echo $mission_text; }?></p>
-				</div><!--.about-banner-text-box-->
-			</section><!--.about-intro-->
+
 
 			<section class="about-history">
 				<h2>History</h2>

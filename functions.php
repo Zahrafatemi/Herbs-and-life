95c735baa22fccf6d29dc5095c912277c0fdf4e7
Donna Dominguez Gamboa <56888091+dm-gamboa@users.noreply.github.com>
2020-03-13 01:15:18 -0700

    Remove JetPack compatibility from functions.php

--- functions.php
+++ functions.php
@@ -149,13 +149,6 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/register-cpt.php';
 
 /**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
-/**
  * Load WooCommerce compatibility file.
  */
 if ( class_exists( 'WooCommerce' ) ) {

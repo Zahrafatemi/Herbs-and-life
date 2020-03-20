a1c1682f4aab3e30630958d2913fcfb5c1b686a4
Kay Watanabe <kay.watanabe80@gmail.com>
2020-03-19 23:25:00 -0700

    Adjust footer styling

--- footer.php
+++ footer.php
@@ -25,67 +25,69 @@
 	</div><!-- #content -->
 
 
-	<footer id="colophon" class="site-footer">
-	<div class="footer-mqm-wrapper">
-			<div class="bottom-logo">
-				<?php if( function_exists( 'get_field' ) ){
-					if( get_field( 'bottom_logo', 'option' ) ){
-						$image = get_field('bottom_logo', 'option');
-						$thumb = wp_get_attachment_image_src($image,'medium');
-					}
-				} ?>
-				<img src="<?php if($thumb[0]) { echo esc_url( $thumb[0]);} ?>"/>
-			</div>
-			<div class = "company-info">
-				<ul>
-					<li><?php the_field('company_name', 'option'); ?></li>
-					<li><?php the_field('company_address', 'option'); ?></li>
-					<li><?php the_field('company_phone_number', 'option'); ?></li>
-					<li><?php the_field('company_email', 'option'); ?></li>
-				</ul>
-			</div>
-		</div><!-- .footer-mqm-wrapper -->
-		<div class="site-info">
-				<div class="footer-menus">
-						<nav id="footer-navigation1" class="footer-navigation">
-							<h3>Order & Support</h3>
-							<?php 
-								wp_nav_menu(
-									array(
-											'theme_location' => 'fotter-1',
-											'memu_id' =>'footer1-menu'
-									)
-								);
-							?>
-						</nav>
-						<nav id="footer-navigation2" class="footer-navigation">
-							<h3>About US</h3>
-							<?php 
-								wp_nav_menu(
-									array(
-											'theme_location' => 'fotter-2',
-											'memu_id' =>'footer2-menu'
-									)
-								);
-							?>
-						</nav>
-					</div><!-- .footer-menu -->
-					<div class="footer-nav-wrapper-left">
-						<nav id="social-navigation" class="social-navigation">
-							<?php 
-								wp_nav_menu(
-									array(
-											'theme_location' => 'social-footer-menu',
-											'memu_id' =>'social-footer-menu',
-											
-									)
-								);
-							?>
-						</nav>
-					
-					<p>© HERB&LIFE <?php echo date("Y"); ?></p>
-					</div><!-- .footer-nav-wrapper-left -->
-		</div><!-- .site-info -->
+	<footer id="colophon">
+		<div class="footer-wrapper site-footer">
+			<div class="footer-mqm-wrapper">
+				<div class="bottom-logo">
+					<?php if( function_exists( 'get_field' ) ){
+						if( get_field( 'bottom_logo', 'option' ) ){
+							$image = get_field('bottom_logo', 'option');
+							$thumb = wp_get_attachment_image_src($image,'medium');
+						}
+					} ?>
+					<img src="<?php if($thumb[0]) { echo esc_url( $thumb[0]);} ?>"/>
+				</div><!-- .bottom-logo -->
+				<div class = "company-info">
+					<ul>
+						<li><?php the_field('company_name', 'option'); ?></li>
+						<li><?php the_field('company_address', 'option'); ?></li>
+						<li><?php the_field('company_phone_number', 'option'); ?></li>
+						<li><?php the_field('company_email', 'option'); ?></li>
+					</ul>
+				</div><!-- .company-info -->
+			</div><!-- .footer-mqm-wrapper -->
+			<div class="site-info">
+					<div class="footer-menus">
+							<nav id="footer-navigation1" class="footer-navigation">
+								<h3>Order & Support</h3>
+								<?php 
+									wp_nav_menu(
+										array(
+												'theme_location' => 'fotter-1',
+												'memu_id' =>'footer1-menu'
+										)
+									);
+								?>
+							</nav>
+							<nav id="footer-navigation2" class="footer-navigation">
+								<h3>About US</h3>
+								<?php 
+									wp_nav_menu(
+										array(
+												'theme_location' => 'fotter-2',
+												'memu_id' =>'footer2-menu'
+										)
+									);
+								?>
+							</nav>
+						</div><!-- .footer-menu -->
+						<div class="footer-nav-wrapper-left">
+							<nav id="social-navigation" class="social-navigation">
+								<?php 
+									wp_nav_menu(
+										array(
+												'theme_location' => 'social-footer-menu',
+												'memu_id' =>'social-footer-menu',
+												
+										)
+									);
+								?>
+							</nav>
+						
+						<p>© HERB&LIFE <?php echo date("Y"); ?></p>
+						</div><!-- .footer-nav-wrapper-left -->
+			</div><!-- .site-info -->
+		</div><!-- .footer-wrapper -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 

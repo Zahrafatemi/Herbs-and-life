c9d15b46c1ee83ff2a9bab6e1b9ba924918ef803
Kyungmi Moon <kyungmimoondesign@gmail.com>
2020-04-13 23:22:11 -0700

    Add related post on blog post

--- single.php
+++ single.php
@@ -25,6 +25,44 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
+		<section class="related-posts">
+			<h2>Related Posts</h2>
+
+			<div class="posts-container">
+				<?php
+				$args = array(
+					'posts_per_page' => 3,
+					'post__not_in'   => array( get_the_ID() ), // Exclude current post
+					'no_found_rows'  => true,
+				);
+
+				// Check for current post category and add tax_query to the query arguments
+				$cats = wp_get_post_terms( get_the_ID(), 'category' ); 
+				$cats_ids = array();  
+				foreach( $cats as $wpex_related_cat ) {
+					$cats_ids[] = $wpex_related_cat->term_id; 
+				}
+				if ( ! empty( $cats_ids ) ) {
+					$args['category__in'] = $cats_ids;
+				}
+				// Query posts
+				$wpex_query = new wp_query( $args );
+
+				// Loop through posts
+				foreach( $wpex_query->posts as $post ) : setup_postdata( $post ); ?>
+					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>">
+						<h3><?php the_title(); ?></h3>
+						<figure class="related-image">
+							<?php the_post_thumbnail( 'blog-thumbnail' ); ?>
+						</figure>
+					</a>
+				<?php
+				endforeach;
+				wp_reset_postdata(); ?>
+
+			</div><!--.posts-container-->
+		</section><!--.related-posts-->
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
